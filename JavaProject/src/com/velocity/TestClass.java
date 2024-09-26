4f28f3d4039628783bbc553ef12b0142e9e2c6b9
Avishkar <avishkaryadav2001@gmail.com>
2024-09-26 10:57:00 +0530

    Simple arraylist code added

---
+++ JavaProject/src/com/velocity/TestClass.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("Mumbai");
+		al.add("Pune");
+		al.add("Satara");
+		al.add("Banglore");
+		System.out.println(al);
+	}
+}
