b8debb053b58f3c87141d9a35ba9aa2f7258600e
Avishkar <avishkaryadav2001@gmail.com>
2024-09-26 11:19:45 +0530

    multiplication code is added

---
+++ JavaProject/src/com/velocity/TestClass2.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+public class TestClass2 {
+
+	
+	public static void main(String[] args) {
+		
+		int a=5;
+		int b=100;
+		int c=a*b;
+		System.out.println("Addition= "+c);
+	}
+}
