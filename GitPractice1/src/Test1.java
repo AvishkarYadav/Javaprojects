44eb4a0f2253161ac576006e4f70521f10ef68c0
Avishkar <avishkaryadav2001@gmail.com>
2024-10-22 13:36:05 +0530

    More object added

--- GitPractice1/src/Test1.java
+++ GitPractice1/src/Test1.java
@@ -5,6 +5,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		ArrayList a=new ArrayList();
 		a.add("avi");
-		a.add("mange");
+		a.add("mangesh");
+		a.add("Akshay");
+		a.add("Vilas");
+		
 	}
 }
