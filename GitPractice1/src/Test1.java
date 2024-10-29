400b9c6fefb060a748e943601d247a7eac8deedd
Avishkar <avishkaryadav2001@gmail.com>
2024-10-29 21:17:57 +0530

    statement added

--- GitPractice1/src/Test1.java
+++ GitPractice1/src/Test1.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 public class Test1 {
 
 	public static void main(String[] args) {
-		ArrayList a=new ArrayList();
+		ArrayList<String> a=new ArrayList<String>();
 		a.add("avi");
 		a.add("mangesh");
 		a.add("Akshay");
 		a.add("Vilas");
-		
+		System.out.println(a);
 	}
 }
